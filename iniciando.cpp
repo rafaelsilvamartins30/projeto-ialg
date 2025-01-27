79839585840e5d9e521421e250b5085ab15e1389
Rafael Martins <rafael.008@gmail.com>
2025-01-27 16:01:22 -0300

    a

--- iniciando.cpp
+++ iniciando.cpp
@@ -12,16 +12,28 @@ struct sportlist{
 	float altura;
 	int peso;
 	char modalidade [20];
+
 	void imprime() {
-        cout << "-----------------------------------------" << endl
-			 << "Nome do Jogador: " << nome << endl
-             << "Idade do Jogador: " << idade << " Anos" << endl
-             << "Nacionalidade do Jogador: " << nacionalidade << endl
-             << "Altura do Jogador: " << altura << " M" << endl
-             << "Peso do Jogador: " << peso << " KG" << endl
-             << "Modalidade do Jogador: " << modalidade << endl;
-	}
+        cout << "_____________________________________________" << endl;
+        cout << " Nome do Jogador: " << nome << endl;
+        cout << " Idade do Jogador: " << idade << endl;
+        cout << " Nacionalidade do Jogador: " << nacionalidade << endl;
+        cout << " Altura do Jogador: " << altura << endl;
+        cout << " Peso do Jogador: " << peso << endl;
+        cout << " Modalidade do Jogador: " << modalidade << endl;
+        cout << "‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾" << endl;
+    }
 };
+
+// Função para limpar a tela do console
+void limparTela() {
+    #ifdef _WIN32
+        system("cls"); // Para Windows
+    #else
+        system("clear"); // Para outros sistemas operacionais
+    #endif
+}
+
 bool lerumaunicalinha(ifstream& entrada, sportlist& algumsportlist){
 	char virgula;
 	return entrada.getline(algumsportlist.nome,30,',')
